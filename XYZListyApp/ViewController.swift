135d950e1577d2b404475b6d61ffec55159599d8
Burak Tunç <burak.tunc@tofas.com.tr>
2020-06-18 00:47:58 +0300

    first commit

---
+++ XYZListyApp/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  XYZListyApp
+//
+//  Created by Burak Tunc on 17.06.2020.
+//  Copyright © 2020 Burak Tunç. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motionManager: CMMotionManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+    }
+    
+    // Completion Handler for startAccelerometerUpdates :)
+    func updateLabels(data:CMAccelerometerData?, error:Error?){
+        guard let accelerometerData = data else { return }
+        
+        // MARK: Number Formatter for FractionDigits
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        // MARK: Each Axis
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y = formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        
+        xLabel.text = "X:\(x)"
+        yLabel.text = "Y:\(y)"
+        zLabel.text = "Z:\(z)"
+    
+    }
+
+}
+
